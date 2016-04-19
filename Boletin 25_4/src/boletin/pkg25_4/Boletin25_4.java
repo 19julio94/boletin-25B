a7546bc2c0934bb7c1de1f87a2c16b7a011a6543
Julio <juliopatriciodasilva@gmail.com>
2016-04-19 09:29:12 +0200

    inicial

---
+++ Boletin 25_4/src/boletin/pkg25_4/Boletin25_4.java
@@ -0,0 +1,17 @@
+
+package boletin.pkg25_4;
+
+/**
+ *
+ * @author JP
+ */
+public class Boletin25_4 {
+
+    
+    public static void main(String[] args) {
+       Ventana sj = new Ventana();
+       
+       sj.setVisible(true);
+    }
+    
+}
